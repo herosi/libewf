4a3d3d7fb54af1ed796591bf4f35bb9c6ef41c87
Hiroshi Suzuki <20717881+herosi@users.noreply.github.com>
2024-05-20 17:55:03 +0900

    Support VS2022 and some other VS to build dokan

--- builddokan.ps1
+++ builddokan.ps1
@@ -5,7 +5,8 @@
 Param (
 	[string]$Configuration = ${Env:Configuration},
 	[string]$Platform = ${Env:Platform},
-	[switch]$UseLegacyVersion = $false
+	[switch]$UseLegacyVersion = $false,
+	[string]$VisualStudioVersion = ""
 )
 
 If (-not ${Configuration})
@@ -17,21 +18,101 @@ If (-not ${Platform})
 	$Platform = "Win32"
 }
 
+If (-Not ${VisualStudioVersion})
+{
+	$VisualStudioVersion = "2022"
+
+	Write-Host "Visual Studio version not set defauting to: ${VisualStudioVersion}" -foreground Red
+}
+If ((${VisualStudioVersion} -ne "2008") -And (${VisualStudioVersion} -ne "2010") -And (${VisualStudioVersion} -ne "2012") -And (${VisualStudioVersion} -ne "2013") -And (${VisualStudioVersion} -ne "2015") -And (${VisualStudioVersion} -ne "2017") -And (${VisualStudioVersion} -ne "2019") -And (${VisualStudioVersion} -ne "2022"))
+{
+	Write-Host "Unsupported Visual Studio version: ${VisualStudioVersion}" -foreground Red
+
+	Exit ${ExitFailure}
+}
+
+$MSBuild = ""
 If (${Env:AppVeyor} -eq "True")
 {
 	$MSBuild = "MSBuild.exe"
 }
-ElseIf (${Env:VisualStudioVersion} -eq "15.0")
+ElseIf (${VisualStudioVersion} -eq "2008")
 {
-	$MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+	$MSBuild = "C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
 }
-ElseIf (${Env:VisualStudioVersion} -eq "9.0")
+ElseIf ((${VisualStudioVersion} -eq "2010") -Or (${VisualStudioVersion} -eq "2012"))
 {
-	$MSBuild = "C:\\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+	$MSBuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 }
-Else
+ElseIf (${VisualStudioVersion} -eq "2013")
 {
-	$MSBuild = "C:\\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+	$MSBuild = "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+}
+ElseIf (${VisualStudioVersion} -eq "2015")
+{
+	$MSBuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+}
+ElseIf (${VisualStudioVersion} -eq "2017")
+{
+	$Results = Get-ChildItem -Path "C:\Program Files\Microsoft Visual Studio\${VisualStudioVersion}\*\MSBuild\15.0\Bin\MSBuild.exe" -Recurse -ErrorAction SilentlyContinue -Force
+
+	If ($Results.Count -eq 0)
+	{
+		$Results = Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\${VisualStudioVersion}\*\MSBuild\15.0\Bin\MSBuild.exe" -Recurse -ErrorAction SilentlyContinue -Force
+	}
+	If ($Results.Count -gt 0)
+	{
+		$MSBuild = $Results[0].FullName
+	}
+}
+ElseIf (${VisualStudioVersion} -eq "2019" -Or ${VisualStudioVersion} -eq "2022")
+{
+	$Results = Get-ChildItem -Path "C:\Program Files\Microsoft Visual Studio\${VisualStudioVersion}\*\MSBuild\Current\Bin\MSBuild.exe" -Recurse -ErrorAction SilentlyContinue -Force
+
+	If ($Results.Count -eq 0)
+	{
+		$Results = Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\${VisualStudioVersion}\*\MSBuild\Current\Bin\MSBuild.exe" -Recurse -ErrorAction SilentlyContinue -Force
+	}
+	If ($Results.Count -gt 0)
+	{
+		$MSBuild = $Results[0].FullName
+	}
+}
+If (-Not ${MSBuild})
+{
+	Write-Host "Unable to determine path to msbuild.exe" -foreground Red
+
+	Exit ${ExitFailure}
+}
+ElseIf (-Not (Test-Path ${MSBuild}))
+{
+	Write-Host "Missing msbuild.exe: ${MSBuild}" -foreground Red
+
+	Exit ${ExitFailure}
+}
+
+
+$PlatformToolset = ""
+
+If (-Not ${PlatformToolset})
+{
+	If (${VisualStudioVersion} -eq "2015")
+	{
+		$PlatformToolset = "v140"
+	}
+	ElseIf (${VisualStudioVersion} -eq "2017")
+	{
+		$PlatformToolset = "v141"
+	}
+	ElseIf (${VisualStudioVersion} -eq "2019")
+	{
+		$PlatformToolset = "v142"
+	}
+	ElseIf (${VisualStudioVersion} -eq "2022")
+	{
+		$PlatformToolset = "v143"
+	}
+	Write-Host "PlatformToolset not set defauting to: ${PlatformToolset}"
 }
 $MSBuildOptions = "/verbosity:quiet /target:Build /property:Configuration=${Configuration},Platform=${Platform}"
 
